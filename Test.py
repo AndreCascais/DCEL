16282a2e4d58070546e6e01d03b010bbaecf8f9c
Raul Ferreira <raulmendesferreira@gmail.com>
2017-03-31 18:29:17 +0100

    Much better Test file now.

--- Test.py
+++ Test.py
@@ -1,15 +1,19 @@
 import pydcel.io
+import sys
 
-file_list = ['sampledata/colinear.ply', 'sampledata/colinearVertical.ply', 'sampledata/hGrid.ply',
-             'sampledata/lTest.ply', 'sampledata/mygrid.ply', 'sampledata/mygrid2.ply', 'sampledata/ricTest.py',
-             'sampledata/ricTestColinear.py', 'sampledata/squares.py', 'sampledata/simplegeom.py']
+if len(sys.argv) == 2:
+    chosen_file = sys.argv[1]
 
+else:
+    file_list = ['sampledata/colinear.ply', 'sampledata/colinearVertical.ply', 'sampledata/hGrid.ply',
+                 'sampledata/lTest.ply', 'sampledata/mygrid.ply', 'sampledata/mygrid2.ply', 'sampledata/ricTest.ply',
+                 'sampledata/ricTestColinear.ply', 'sampledata/squares.ply', 'sampledata/simplegeom.ply']
 
-for f in file_list:
-    print(f)
-print("")
+    for f in file_list:
+        print(f)
+    print("")
 
-chosen_file = input("which file do you want: ",)
+    chosen_file = input("which file do you want: ",)
 
 d = pydcel.io.ply2dcel(chosen_file)
 
